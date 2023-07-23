e262797d5273b71cdd735bf9c12d5d6fbb35763c
RazelRaz <razel16dcm@gmail.com>
2023-07-23 14:31:01 +0600

    file and folder structure ready

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-outline btn-info">Info</button>
+<button className="btn btn-outline btn-success">Success</button>
+<button className="btn btn-outline btn-warning">Warning</button>
+<button className="btn btn-outline btn-error">Error</button>
+    </div>
+  );
+};
+
+export default App;
